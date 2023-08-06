a255489f4e6a6bf717ab97e4e9ae32a6765cd651
Olena Havuliak <olena.havuliak@nerdysoft.com>
2023-08-06 21:19:59 +0300

    Refactored code

---
+++ src/main/java/com/example/quizapp/exception/MessageCode.java
@@ -0,0 +1,15 @@
+package com.example.quizapp.exception;
+
+import lombok.Getter;
+
+@Getter
+public enum MessageCode {
+    NOT_FOUND_QUESTION ("Question not found."),
+    NOT_FOUND_QUIZ ("Quiz not found"),
+    NOT_FOUND_OPTION("Option not found");
+
+    private final String message;
+    MessageCode(String message) {
+        this.message = message;
+    }
+}
